0fd5ef71dc2c6b66bb083e71d41acd5fe75ca90f
Akash Karwande <akashnkarwande@gmail.com>
2026-01-24 09:26:03 +0530

    variables difference

--- 1-variables.js
+++ 1-variables.js
@@ -1,5 +1,40 @@
+// var vs let vs const 
 // scope
+// {
+//     const a = 2;
+// }
+// console.log(a);
 // shadowing
+// var b = 10;
+
+// {
+//     let b = 20;
+//     // console.log(b);
+// }
+
+// console.log(b);
+
+
 // Declaration
+// var a;
+// a = 0;
+// let b;
+// b = 2;
+// const c = 12;
 // Reinitialization
+// var varVariable = 30;
+// varVariable = 50;
+
+// let letVariable = 'Hi';
+// letVariable = 40;
+
+// const constVar = 'URL';
+
+// constVar = "12";
 // Hoisting
+
+console.log(y);
+var abc = 12;
+
+let x = 'Hi';
+const y = 20;
