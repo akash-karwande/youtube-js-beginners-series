4698ca26ff6acf60271734b01c0edeeed3f6e9a2
Akash Karwande <akashnkarwande@gmail.com>
2026-02-01 22:02:04 +0530

    async await

---
+++ 8-asyncAwait.js
@@ -0,0 +1,54 @@
+let url = "https://dummyjson.com/users/";
+
+async function getData(id) {
+    try {
+        let response = await fetch(url + id);
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// getData(1);
+
+function getUser() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Akash");
+            // reject("service is down")
+        }, 1000)
+    })
+}
+
+function getOrder() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("order fetched");
+        }, 1000)
+    })
+}
+
+function sendEmail() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("sent");
+        }, 1000)
+    })
+}
+
+async function getAllUserData() {
+    try {
+        let name = await getUser();
+        let order = await getOrder();
+        let emailStatus = await sendEmail();
+        console.log(name, order, emailStatus);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+console.log("before calling")
+getAllUserData();
+console.log("after calling")
+
